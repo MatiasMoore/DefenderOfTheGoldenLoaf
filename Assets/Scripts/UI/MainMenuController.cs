c93bf0c48b1d63841f285afd6822eabff8814b86
Chernobaev Egor <104233247+RR3Z@users.noreply.github.com>
2024-03-31 08:13:26 +0300

    refactor(project): renamed UIControler script

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -4,7 +4,7 @@ using UnityEngine.UIElements;
 
 namespace DefenderOfTheGoldenLoaf.UI
 {
-    public class UIController : MonoBehaviour
+    public class MainMenuController : MonoBehaviour
     {
         private VisualElement _root;
 
