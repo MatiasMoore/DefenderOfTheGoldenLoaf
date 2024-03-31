821f32a7cada788b02aea566373e033d9c57c37c
Zverkov Maxim <tlore007@gmail.com>
2024-03-31 07:31:36 +0300

    feat(Frog): now from moving with jumps

--- Assets/Scripts/Frog/AI/States/Moving/EscapeFrogState.cs
+++ Assets/Scripts/Frog/AI/States/Moving/EscapeFrogState.cs
@@ -6,7 +6,18 @@ public class EscapeFrogState : MovingAiPrimitive
 {
     [SerializeField]
     private float _minDistanceToTarget;
+    [SerializeField]
+    private float _jumpCooldown = 1f;
+    [SerializeField]
+    private float _jumpTime = 0.4f;
 
+    private Timer _timer;
+    private enum JumpState
+    {
+        Jumping,
+        NotJumping
+    }
+    private JumpState _state;
     public override void DebugDrawGizmos()
     {
         
@@ -19,7 +30,10 @@ public class EscapeFrogState : MovingAiPrimitive
 
     public override void StartState()
     {
-        
+        _timer = new Timer(_jumpTime);
+        _state = JumpState.Jumping;
+        _timer.OnTimerDone += StopJump;
+        _timer.StartTimer();
     }
 
     public override void Stop()
@@ -29,7 +43,7 @@ public class EscapeFrogState : MovingAiPrimitive
 
     public override void UpdateState()
     {
-        GetMovingStateManager().movingAIStateParam.objectMovement.GoToPointOnNavMesh(GetMovingStateManager().movingAIStateParam.escapeTarget.position);
+        _timer.Tick();
 
         if (Vector3.Distance(GetMovingStateManager().movingAIStateParam.escapeTarget.position, GetMovingStateManager().movingAIStateParam.objectMovement.transform.position) < 1f)
         {
@@ -37,5 +51,44 @@ public class EscapeFrogState : MovingAiPrimitive
             GetMovingStateManager().Escaped();
             GetMovingStateManager().SwitchToState("MoveToTarget");
         }
+
+        switch (_state)
+        {
+            case JumpState.Jumping:
+                Jumping();
+                break;
+            case JumpState.NotJumping:
+                NotJumping();
+                break;
+        }
+    }
+
+    private void StartJump()
+    {
+        _timer.OnTimerDone -= StartJump;
+        _state = JumpState.Jumping;
+        _timer = new Timer(_jumpTime);
+        _timer.StartTimer();
+        _timer.OnTimerDone += StopJump;
+
+    }
+
+    private void StopJump()
+    {
+        _timer.OnTimerDone -= StopJump;
+        _state = JumpState.NotJumping;
+        _timer = new Timer(_jumpCooldown);
+        _timer.StartTimer();
+        _timer.OnTimerDone += StartJump;
+    }
+
+    private void Jumping()
+    {
+        GetMovingStateManager().movingAIStateParam.objectMovement.GoToPointOnNavMesh(GetMovingStateManager().movingAIStateParam.escapeTarget.position);
+    }
+
+    private void NotJumping()
+    {
+        GetMovingStateManager().movingAIStateParam.objectMovement.Stop();
     }
 }
