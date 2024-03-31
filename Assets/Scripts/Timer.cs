4d523d7889af2ac3f5323680eeda5223d91f80f9
Chernobaev Egor <104233247+RR3Z@users.noreply.github.com>
2024-03-31 07:59:50 +0300

    feat(Timer: added new methods for visual display

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,15 @@ public class Timer
     private float _startTime;
     private float _duration;
     private float _targetTime;
+    private float _currentTime;
 
     private bool isActive;
     public Timer(float duration)
     {
         _duration = duration;
     }
+
+    public float GetTime => _currentTime;
 
     public void StartTimer()
     {
@@ -41,7 +45,23 @@ public class Timer
             StopTimer();
             OnTimerDone?.Invoke();      
         }
+
+        _currentTime = _targetTime - Time.time;
     }
 
+    public string FloatToString(float time)
+    {
+        TimeSpan timeSpan = TimeSpan.FromSeconds(time);
+        return string.Format("{0:00}:{1:00}", (int)timeSpan.TotalMinutes, timeSpan.Seconds);
+    }
 
+    public float StringToFloat(string time)
+    {
+        string[] parts = time.Split(':');
+
+        float minutes = int.Parse(parts[0]); // Int to float, hmmmmm.... :)
+        float seconds = int.Parse(parts[1]);
+
+        return minutes * 60 + seconds;
+    }
 }
